9888dd14e4845efb618d353f3b4253b0f8c2a869
jerinraj04 <42570606+jerinraj04@users.noreply.github.com>
2018-09-19 15:34:54 +0530

    Update jerin11.py

--- jerin11.py
+++ jerin11.py
@@ -1,8 +1,8 @@
-def power(base,exp):
+def power(base):
     if(exp==1):
         return(base)
     if(exp!=1):
         return(base*power(base,exp-1))
 base=int(input("Enter base: "))
-exp=int(input("Enter exponential value: "))
+exp=int(input("enter exponential value: "))
 print("result:",power(base,exp))
