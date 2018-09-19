e93db8bdc8924ad92724a7b61f590e0f3c19ae53
jerinraj04 <42570606+jerinraj04@users.noreply.github.com>
2018-09-19 14:38:34 +0530

    Update jerin1.py

--- jerin1.py
+++ jerin1.py
@@ -1,8 +1,8 @@
-l = input("Input a letter of the alphabet: ")
+1 = input("Input a letter of the alphabet: ")
 
-if l in ('a', 'e', 'i', 'o', 'u'):
+if 1 in ('a', 'e', 'i', 'o', 'u'):
 	print("%s is a vowel." % l)
-elif l == 'y':
-	print("Sometimes letter y stand for vowel, sometimes stand for consonant.")
+elif 1 == 'y':
+	print("sometimes letter y stand for vowel, sometimes stand for consonant.")
 else:
-	print("%s is a consonant." % l) 
+	print("%s is a vowel." % 1)
