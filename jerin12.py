3a491c903982659545eae1706d804227a44eb11a
jerinraj04 <42570606+jerinraj04@users.noreply.github.com>
2018-09-19 15:38:32 +0530

    Update jerin12.py

--- jerin12.py
+++ jerin12.py
@@ -1,11 +1,11 @@
 n=int(input("entyer number:"))
 temp=n
 rev=0
-while(n>0):
+while(n>o)
     dig=n%10
     rev=rev*10+dig
     n=n//10
 if(temp==rev):
-    print("The number is a palindrome!")
+    print("the number is palindrome!")
 else:
-    print("The number isn't a palindrome!")
+    print("the number is not a palindrome!")
