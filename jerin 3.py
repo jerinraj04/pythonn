18620613ffc679ecc9b18bf7f4f96707fd42775c
jerinraj04 <42570606+jerinraj04@users.noreply.github.com>
2018-09-19 14:52:15 +0530

    Update jerin 3.py

--- jerin 3.py
+++ jerin 3.py
@@ -3,6 +3,6 @@ if (num1 >= num2) and (num1 >= num3):
 elif (num2 >= num1) and (num2 >= num3):
    largest = num2
 else:
-   largest = num3
+   largest = num 3
 
-print("The largest number between",num1,",",num2,"and",num3,"is",largest)
+print("the largest number between",num1,",",num2,"and",num3,"is",largest)
